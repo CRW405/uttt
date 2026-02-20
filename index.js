268a5f4347be83863604ca22530f6f77c3153c94
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-02-19 19:23:50 -0600

    added the psuedo code for data modeling

---
+++ index.js
@@ -0,0 +1,19 @@
+// let l "levels" = int >= 1
+//
+// let sp "simple Board" = [arr of length 9 with int (-1,0,1)]
+//
+// let b(l=1) "utt board"  = [arr of length 9 with sp ]
+// let p(l=1) "utt place"  = (0-8)
+// let w      "utt win"    = [arr of length 9 with (-1,0,1)]
+//
+// let b(l=2) "uutt board"   = [arr of length 9 containing b(l=1)]
+// let p(l=2) "uutt places"  = [arr of length 2 with int (0-8)]
+// let w(l=2) "uutt wins"    = [arr of length 2 with [arr of length 9 with (-1, 0, 1)]]
+//
+// let b(l=n) "recursive board"   = [arr of length 9 containing b(l=n--)]
+// let p(l=n) "recursive places"  = [arr of length n with int (0-8)]
+// let w(l=n) "recursive wins"    = [arr of length n with [arr of length 9 with (-1, 0, 1)]]
+//
+// func determine_winner(board, pos) {
+//
+// }
