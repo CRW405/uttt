5eff45c54f62413cd6ea19efb9569abd4f796a5f
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-03-09 21:20:35 -0500

    added rules for each level of game for psuedo coding reasons

--- logic.js
+++ logic.js
@@ -34,6 +34,29 @@
 // }
 //
 // use determine_winner() to recursively determine wins that can determine placements on the super board
+//
+// Tic-Tac-Toe (l=1):
+// 1. 3x3 board in which players can place an X or O.
+// 2. Players must alternate placing respective marks, marks cannot be placed on the same cell
+// 3. The first player to get three of their marks in a straight or diagonal line wins
+//
+// Ultimate Tic-Tac-Toe (l=2):
+// 1. 3x3 board, each cell containing their own 3x3 board in which players can place either an X or O in the sub board
+// 2. Players alternate placing respective marks in the sub boards
+// 3. When a player places a mark on a sub cell in a sub board, the next player must play the respective sub board on the higher super board
+// 4. All normal Tic-Tac-Toe rules apply to the sub board, when a board is won, it is analogous to placing the winning mark over the whole cell
+// 5. If a player places a mark that would lead to the next player playing in a board that has already been won / claimed,
+// this is treated as a wildcard and the player gets to choose where they want to play, all previous rules applying
+// 6. The first player who gets three super cells claimed in a straight or diagonal line wins
+//
+// Recursive Tic-Tac-Toe (l=n):
+// 1. 3x3 board containing another 3x3 board, recursing for n-1 times. There are 3x3 normal sub boards at the bottom of the stack, at the depth of n
+// 2. Players alternate placing respective marks in the sub boards
+// 3. when a player places a mark at n (the sub boards), then the next player will have to play the corresponding board, eg,
+// x marks ...3-6-2-5, then the next player will have to play at the corresponding sub board of ...3-6-5
+// 4. The above rule also applies to winning a board, eg, x wins ...3-6-5 then o will have to play in ...3-5
+// 5. Blocking / Wildcard rules apply to super boards like before
+// 6. The first player to get three super cells of level 1 in a straight or diagonal wins the whole game
 
 function create_board(l) {
   // console.log("Running for l = " + l);
